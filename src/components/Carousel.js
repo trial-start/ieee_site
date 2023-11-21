4c7afefd600c92bbef5608d6b7e5d1d1d0ddbd5b
trial-start <endrapu.saisushanth2003@gmail.com>
2023-11-22 00:58:39 +0530

    style

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,38 +1,26 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
 
-const ImageCarousel = ({ val, imgs = ["tem.jpg", "temp.jpg"] }) => {
+const ImageCarousel = ({ val, imgs = ["t_logo.jpg", "temp.jpg"] }) => {
   const carouselStyle = {
     paddingTop: val === "x" ? "" : "65px", // Adjust the value based on your NavBar height
   };
 
   return (
-    <Carousel style={carouselStyle} interval={3000} className="custom1">
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={`images/${imgs[0]}`}
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>Slide 1</h3>
-          <p>Description for Slide 1</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={`images/${imgs[1]}`}
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h3>Slide 2</h3>
-          <p>Description for Slide 2</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-
-      {/* Add more Carousel items as needed */}
+    <Carousel style={carouselStyle} interval={3000} className="custom1" wrap>
+      {imgs.map((img, index) => (
+        <Carousel.Item key={index}>
+          <img
+            className="d-block w-100"
+            src={`images/${img}`}
+            alt={`Slide ${index + 1}`}
+          />
+          <Carousel.Caption>
+            <h3>{`Slide ${index + 1}`}</h3>
+            <p>{`Description for Slide ${index + 1}`}</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      ))}
     </Carousel>
   );
 };
