252f977a1954a8e309900a822fa417597f7d61ea
trial-start <endrapu.saisushanth2003@gmail.com>
2023-12-03 00:28:14 +0530

    v2.8

--- src/components/CoreTeam.js
+++ src/components/CoreTeam.js
@@ -2,16 +2,16 @@ import React from "react";
 import { Row } from "react-bootstrap";
 import Member from "./Member";
 
-const CoreTeam = ({ num = "5" }) => {
+const CoreTeam = ({ num = "2" }) => {
   const members = Array.from({ length: num }, (_, index) => index + 1);
   console.log(members);
 
   return (
-    <Row>
+    <Row className="custom-disp">
       {members.map((index) => (
         <Member
           key={index}
-          img={`https://via.placeholder.com/100x100`}
+          img={`images/ckts/img${index - 1}.jpg`}
           name={`Member ${index}`}
           description={`Position ${index}`}
         />
--- src/components/Member.js
+++ src/components/Member.js
@@ -1,11 +1,19 @@
 import { Col, Image } from "react-bootstrap";
 export default function Member({ img, name, description = "fejg" }) {
+  console.log(img);
   return (
     <Col md={4} className="mb-4">
       <div style={{ textAlign: "center" }}>
-        <Image src={img} roundedCircle style={{ float: "left" }} />
+        <Image
+          src={img}
+          roundedCircle
+          style={{ float: "left", height: "18vh", width: "18vh" }}
+        />
       </div>
-      <div style={{ marginLeft: "130px", marginTop: "20px" }}>
+      <div
+        className="cust-ml"
+        style={{ marginLeft: "140px", marginTop: "42px" }}
+      >
         <p style={{ marginTop: "10px" }}>{name}</p>
         <p style={{ marginTop: "5px" }}>{description}</p>
       </div>
