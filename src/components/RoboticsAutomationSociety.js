b62c0558474855e97cc7b45dd2cfad359bcf8718
AksharaAnnam <annamakshara@gmail.com>
2024-01-24 16:58:02 +0530

    new

--- src/components/RoboticsAutomationSociety.js
+++ src/components/RoboticsAutomationSociety.js
@@ -6,16 +6,12 @@ import Carousel from "./RASCarousel";
 import Events from "./Events";
 
 const RoboticsAutomationSociety = ({ imgs, num }) => {
-  const newImages = [
-    "A:\IEEE\ieee_site\public\images\robo_soc.jpg",
-    "A:\IEEE\ieee_site\public\images\ras.png",
-  ];
   return (
     <div>
       <NavBar itemId="about" itemName="About" teamType="Team" />
       <section className="society-section" id="about">
         <h2 className="section-heading">Robotics and Automation Society</h2>
-        <Carousel val="x" imgs={newImages} />
+        <Carousel val="x" imgs={imgs} />
         <p className="section-description" style={{ textAlign: "justify" }}>
         The IEEE Robotics and Automation Society (RAS) is a prominent professional organization operating under the umbrella of the Institute of Electrical and Electronics Engineers (IEEE). Established to propel the theory and application of robotics and automation, RAS engages in a multifaceted approach to advance the field. The society actively supports research and development by organizing conferences, workshops, and publications, facilitating the exchange of cutting-edge ideas and discoveries among its members. Education and outreach efforts are also a key focus, with RAS contributing to educational programs, providing resources for students and educators, and organizing events to raise awareness and interest in robotics and automation. Additionally, RAS plays a pivotal role in setting standards and best practices, ensuring the reliability and safety of robotic systems. By fostering a global network of professionals, researchers, and students, RAS serves as a hub for collaboration, offering opportunities for networking and knowledge exchange. Through its commitment to excellence, the IEEE Robotics and Automation Society continues to shape the future of robotics and automation, influencing technological innovation and promoting the widespread adoption of these technologies across various industries.
         </p>
