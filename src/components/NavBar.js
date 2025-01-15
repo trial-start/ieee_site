23cec5d6fb13e24c0c923fb0fa584c896c6c2948
Sreesha-Reddy <sreeshareddy19@gmail.com>
2025-01-15 17:52:28 +0530

    core

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -66,7 +66,7 @@ const NavBar = ({ itemId, itemName, teamType, val }) => {
           </Nav.Link>
           <Nav.Link href="#coreteam" className="custom-margin">
             {/* {teamType} */}
-            Core Team
+            Core 
           </Nav.Link>
           {val !== "x" && (
             <Nav.Link href="#events" className="custom-margin">
